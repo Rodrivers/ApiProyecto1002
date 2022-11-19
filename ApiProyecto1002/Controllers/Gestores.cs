ab73ebdf6f03bd1de042dab6a502561affef4f79
Rodrivers <rogerleon400@gmail.com>
2022-11-19 11:28:59 -0600

    Agregar archivos de proyecto.

---
+++ ApiProyecto1002/Controllers/Gestores.cs
@@ -0,0 +1,120 @@
+﻿using ApiProyecto1002.Context;
+using ApiProyecto1002.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiProyecto1002.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Gestores : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public Gestores(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        // GET: api/<Gestores>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(context.usuarios.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+       
+
+        // GET api/<Gestores>/5
+        [HttpGet("{id}", Name="Usuarios")]
+
+       
+        public ActionResult Get(int id)
+        {
+            try
+            {
+                var user=context.usuarios.FirstOrDefault(x => x.Id == id);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST api/<Gestores>
+        [HttpPost]
+        public ActionResult Post([FromBody]usuarios user)
+        {
+            try
+            {
+               context.usuarios.Add(user);
+                context.SaveChanges();
+                return CreatedAtRoute("Usuarios",new { id = user.Id },user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        // PUT api/<Gestores>/5
+        [HttpPut("{id}")]
+
+
+        public ActionResult Put(int id, [FromBody] usuarios user)
+        {
+            try
+            {
+                if (user.Id == id)
+                {
+                    context.Entry(user).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return CreatedAtRoute("Usuarios", new { id = user.Id }, user);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+               }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<Gestores>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var user = context.usuarios.FirstOrDefault(x => x.Id == id);
+                if (user != null)
+                {
+                   context.usuarios.Remove(user);
+                    context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ ApiProyecto1002/Startup.cs
@@ -0,0 +1,26 @@
+﻿using ApiProyecto1002.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProyecto1002
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigurationServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<AppContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("Conexion")));
+        }
+        public void Configure (IApplicationBuilder app, IHostApplicationLifetime lifetime)
+        {
+
+        }
+    }
+    }
+}
